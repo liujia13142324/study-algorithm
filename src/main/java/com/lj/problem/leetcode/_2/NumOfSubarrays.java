8b1f3b9fbe044f738d0a94651b3c969fab567450
wangdamao <524294514@qq.com>
2025-08-03 18:15:13 +0800

    [Question Salved] [来源]：leetcode [题目]：1343. 大小为 K 且平均值大于等于阈值的子数组数目 [难度]：中等 [思路]：双指针，滑窗（固定窗口）

---
+++ src/main/java/com/lj/problem/leetcode/_2/NumOfSubarrays.java
@@ -0,0 +1,61 @@
+package com.lj.problem.leetcode._2;
+
+import org.junit.Test;
+
+/**
+ * 1343. 大小为 K 且平均值大于等于阈值的子数组数目
+ * 给你一个整数数组 arr 和两个整数 k 和 threshold 。
+ * 请你返回长度为 k 且平均值大于等于 threshold 的子数组数目。
+ *
+ *
+ *
+ * 示例 1：
+ * 输入：arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+ * 输出：3
+ * 解释：子数组 [2,5,5],[5,5,5] 和 [5,5,8] 的平均值分别为 4，5 和 6 。其他长度为 3 的子数组的平均值都小于 4 （threshold 的值)。
+ *
+ * 示例 2：
+ * 输入：arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5
+ * 输出：6
+ * 解释：前 6 个长度为 3 的子数组平均值都大于 5 。注意平均值不是整数。
+ *
+ *
+ * 提示：
+ * 1 <= arr.length <= 105
+ * 1 <= arr[i] <= 104
+ * 1 <= k <= arr.length
+ * 0 <= threshold <= 104
+ */
+public class NumOfSubarrays {
+
+
+    @Test
+    public void test() {
+        System.out.println(numOfSubarrays(new int[]{2,2,2,2,5,5,5,8}, 3, 4));
+    }
+
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int l = 0, r = 0, sum = 0, c = 0, len = arr.length;
+        double kb = k;
+
+        while (r < k) {
+            sum += arr[r];
+            r++;
+        }
+
+        if (sum / kb >= threshold) {
+            c++;
+        }
+
+        while (r < len) {
+            sum += arr[r++];
+            sum -= arr[l++];
+            if (sum / kb >= threshold) {
+                c++;
+            }
+        }
+
+        return c;
+    }
+
+}
