401ac187ad97800628ad056c8c69e465c8d7d977
jia <524294514@qq.com>
2025-08-30 00:21:23 +0800

    [Question Salved] [来源]：leetcode [题目]：各位相加 [难度]：简单 [思路]：

---
+++ src/main/java/com/lj/problem/leetcode/_1/AddDigits.java
@@ -0,0 +1,35 @@
+package com.lj.problem.leetcode._1;
+
+/**
+ * 各位相加
+ * 给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。返回这个结果。
+ *
+ * 示例 1:
+ * 输入: num = 38
+ * 输出: 2
+ * 解释: 各位相加的过程为：
+ * 38 --> 3 + 8 --> 11
+ * 11 --> 1 + 1 --> 2
+ * 由于 2 是一位数，所以返回 2。
+ *
+ * 示例 2:
+ * 输入: num = 0
+ * 输出: 0
+ *
+ * 提示：
+ * 0 <= num <= 231 - 1
+ * 进阶：你可以不使用循环或者递归，在 O(1) 时间复杂度内解决这个问题吗？
+ */
+public class AddDigits {
+
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int tmp = 0;
+            for (long i = 10000000000L; i > 1; i/=10) {
+                tmp += (int) ((num % i)/(i / 10));
+            }
+            num = tmp;
+        }
+        return num;
+    }
+}
