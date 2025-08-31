3f0f1f25f6505f59a9512390f0dc093eba4e63c6
jia <524294514@qq.com>
2025-08-31 23:38:17 +0800

    [Question Salved] [来源]：leetcode [题目]：各位相加 [难度]：简单 [思路]：数学方案，直接 mod 9

--- src/main/java/com/lj/problem/leetcode/_1/AddDigits.java
+++ src/main/java/com/lj/problem/leetcode/_1/AddDigits.java
@@ -1,8 +1,18 @@
 package com.lj.problem.leetcode._1;
 
+import org.junit.Test;
+
 /**
  * 各位相加
  * 给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。返回这个结果。
+ *
+ * 99 -> 18 -> 9
+ * 999 -> 27 -> 9
+ * 999999999 -> 81 -> 9
+ * 1999999999 -> 82 -> 10 -> 1
+ *
+ * 523 -> 10 -> 1
+ * 742 -> 13 -> 4
  *
  * 示例 1:
  * 输入: num = 38
@@ -21,6 +31,32 @@ package com.lj.problem.leetcode._1;
  * 进阶：你可以不使用循环或者递归，在 O(1) 时间复杂度内解决这个问题吗？
  */
 public class AddDigits {
+
+    @Test
+    public void test() {
+        System.out.println(Integer.toBinaryString(38));
+        System.out.println(addDigits2(38));
+    }
+
+    public int addDigits4(int num) {
+        return (num - 1) % 9 + 1;
+    }
+
+    public int addDigits3(int num) {
+        return num < 10 ? num : num%9 == 0 ? 9 : num%9;
+    }
+
+    public int addDigits2(int num) {
+        while (num >= 10) {
+            int tmp = 0;
+            for (int i = 1000000000; i >= 1; i/=10) {
+                tmp += num / i;
+                num = num % i;
+            }
+            num = tmp;
+        }
+        return num;
+    }
 
     public int addDigits(int num) {
         while (num >= 10) {
