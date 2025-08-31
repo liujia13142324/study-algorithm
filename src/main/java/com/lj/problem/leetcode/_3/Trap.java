1dbe1e8a0999a2610e9f3790cbd5b789d819a9e8
jia <524294514@qq.com>
2025-09-01 02:44:35 +0800

    [Question Salved] [来源]：42. 接雨水 [题目]：统计好三元组 [难度]：困难 [思路]：前后缀，双指针

--- src/main/java/com/lj/problem/leetcode/_3/Trap.java
+++ src/main/java/com/lj/problem/leetcode/_3/Trap.java
@@ -21,8 +21,47 @@ public class Trap {
     public void test() {
         System.out.println(trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
         System.out.println(trap(new int[]{4,2,0,3,2,5}));
-        // 23
         System.out.println(trap(new int[]{5,5,1,7,1,1,5,2,7,6}));
+
+        System.out.println(trap2(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+        System.out.println(trap2(new int[]{4,2,0,3,2,5}));
+        System.out.println(trap2(new int[]{5,5,1,7,1,1,5,2,7,6}));
+
+        System.out.println(trap3(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+        System.out.println(trap3(new int[]{4,2,0,3,2,5}));
+        System.out.println(trap3(new int[]{5,5,1,7,1,1,5,2,7,6}));
+    }
+
+    public int trap3(int[] height) {
+        int l = 0, r = height.length - 1, ans = 0 , maxL = 0, maxR = 0;
+        while (l < r) {
+            if (height[l] <= height[r]) {
+                maxL = Math.max(maxL, height[l]);
+                ans += maxL - height[l];
+                l++;
+            }else {
+                maxR = Math.max(maxR, height[r]);
+                ans += maxR - height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+
+    public int trap2(int[] height) {
+        int[] maxL = new int[height.length];
+        int[] maxR = new int[height.length];
+        maxL[0] = height[0];
+        maxR[height.length - 1] = height[height.length - 1];
+        for (int i = 1; i < height.length; i++) {
+            maxL[i] = Math.max(maxL[i - 1], height[i]);
+            maxR[height.length - 1 - i] = Math.max(maxR[height.length - i], height[height.length - 1 - i]);
+        }
+        int ans = 0;
+        for (int i = 0; i < height.length; i++) {
+            ans += Math.min(maxL[i], maxR[i]) - height[i];
+        }
+        return ans;
     }
 
 
