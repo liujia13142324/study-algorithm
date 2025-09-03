a1f086baae274cefe1e98d787151e38f4cdea47d
wangdamao <524294514@qq.com>
2025-09-04 00:28:12 +0800

    [Question Salved] [来源]：leetcode [题目]：5. 最长回文子串 [难度]：中等 [思路]： 双指针，但是效率不高，每次右边需要回到第二次匹配l的位置，没有的话，直接 r--

--- src/main/java/com/lj/problem/leetcode/_2/LongestPalindrome.java
+++ src/main/java/com/lj/problem/leetcode/_2/LongestPalindrome.java
@@ -1,16 +1,78 @@
 package com.lj.problem.leetcode._2;
 
+import org.junit.Test;
+
 /**
- * 最长回文子串
- * 给你一个字符串 s，找到 s 中最长的回文
- * 子串
- * 。
+ * 5. 最长回文子串
+ * 给你一个字符串 s，找到 s 中最长的 回文 子串。
  *
- * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ * 示例 1：
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ *
+ * 示例 2：
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ *
+ *
+ * 提示：
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母组成
  */
 public class LongestPalindrome {
-    
-    
+
+    @Test
+    public void test() {
+        System.out.println(longestPalindrome2("babad"));
+        System.out.println(longestPalindrome2("cbbd"));
+        System.out.println(longestPalindrome2("aacabdkacaa"));
+        System.out.println(longestPalindrome2("bacabab"));
+        System.out.println(longestPalindrome2("xaabacxcabaaxcabaax"));
+        System.out.println(longestPalindrome2("aaabaaaa"));
+
+    }
+
+    public String longestPalindrome2(String s) {
+        char[] charArray = s.toCharArray();
+        int maxL = 0, maxR = 0;
+        for (int i = 0; i < charArray.length - maxR + maxL; i++) {
+            int l = i;
+            int r = charArray.length - 1;
+            int secondLMatched = -1;
+            boolean firstLMatched = false;
+            while (l < r) {
+                if (charArray[r] == charArray[i]) {
+                    if (secondLMatched == -1 && firstLMatched) secondLMatched = r;
+                    if (!firstLMatched) firstLMatched = true;
+                }
+                if (charArray[l] == charArray[r]) {
+                    l++;
+                    r--;
+                }else if (secondLMatched != -1){
+                    l = i;
+                    r = secondLMatched;
+                    firstLMatched = false;
+                    secondLMatched = -1;
+                }else {
+                    l = i;
+                    r--;
+                }
+            }
+            // 汇聚到一点，回文数的就是以 l 为中心， l - i 为半径
+            if (l == r && (l - i) * 2 + 1 > maxR - maxL) {
+                maxL = i;
+                maxR = i + (l - i) * 2;
+            }else if ((l - i) * 2 > maxR - maxL){
+                maxL = i;
+                maxR = i + (l - i) * 2 - 1;
+            }
+        }
+        return s.substring(maxL, maxR + 1);
+    }
+
+
+
     // 中心扩散法
     public String longestPalindrome(String s) {
         int maxLen = 1;
