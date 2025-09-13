950a2e806acc89fd985a9a6d0692ffd6637583b0
wangdamao <524294514@qq.com>
2025-09-13 23:57:40 +0800

    [Question Salved] [来源]：leetcode [题目]：2563. 统计公平数对的数目 [难度]：中等 [思路]：循环+二分，取[l,r] 和 [i+1, len-1]的交集

--- src/main/java/com/lj/problem/leetcode/_2/CountFairPairs.java
+++ src/main/java/com/lj/problem/leetcode/_2/CountFairPairs.java
@@ -1,5 +1,9 @@
 package com.lj.problem.leetcode._2;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * 2563. 统计公平数对的数目
  * 给你一个下标从 0 开始、长度为 n 的整数数组 nums ，和两个整数 lower 和 upper ，返回 公平数对的数目 。
@@ -26,4 +30,75 @@ package com.lj.problem.leetcode._2;
  * -109 <= lower <= upper <= 109
  */
 public class CountFairPairs {
+
+    @Test
+    public void test() {
+        System.out.println(countFairPairs(new int[]{0,1,7,4,4,5}, 3, 6));
+        System.out.println(countFairPairs(new int[]{1,7,9,2,5}, 11, 11));
+    }
+
+    // TODO 双指针
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // 找到第一个 l，使 num[l] >= lower
+            int l = lowBound(nums, i, lower - nums[i]);
+            // 找到第一个 r，使 num[r] <= lower
+            int r = lowBound(nums, i, upper - nums[i] + 1) - 1;
+
+            if (r <= i) {
+                break;
+            }
+
+            ans += Math.max(Math.min(r, nums.length - 1) - Math.max(l, i + 1) + 1, 0) ;
+        }
+
+        return ans;
+    }
+
+    // 开区间
+    public int lowBound(int[] nums, int l, int target) {
+        int r = nums.length;
+        while (l + 1 < r) {
+            int mid = (l + r) >>> 1;
+            if (nums[mid] >= target) {
+                r = mid;
+            }else {
+                l = mid;
+            }
+        }
+        return r;
+    }
+
+
+    /**
+     * 别人答案，比我要慢一些
+     * @return
+     */
+    public long countFairPairs2(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long ans = 0;
+        for (int j = 0; j < nums.length; j++) {
+            // 注意要在 [0, j-1] 中二分，因为题目要求两个下标 i < j
+            int r = lowerBound(nums, j, upper - nums[j] + 1);
+            int l = lowerBound(nums, j, lower - nums[j]);
+            ans += r - l;
+        }
+        return ans;
+    }
+
+    // 原理请看 https://www.bilibili.com/video/BV1AP41137w7/
+    private int lowerBound(int[] nums, int right, int target) {
+        int left = -1;
+        while (left + 1 < right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return right;
+    }
 }
