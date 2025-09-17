2370bc6f8aea45440518ffb119989a452186182c
wangdamao <524294514@qq.com>
2025-09-17 23:12:59 +0800

    [Question Salved] [来源]：leetcode [题目]：101. 对称二叉树 [难度]：简单 [思路]：

---
+++ src/main/java/com/lj/problem/leetcode/_1/IsSymmetric.java
@@ -0,0 +1,48 @@
+package com.lj.problem.leetcode._1;
+
+/**
+ * 101. 对称二叉树
+ * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+ *
+ * 示例 1：
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ *
+ * 提示：
+ * 树中节点数目在范围 [1, 1000] 内
+ * -100 <= Node.val <= 100
+ *
+ * 进阶：你可以运用递归和迭代两种方法解决这个问题吗？
+ */
+public class IsSymmetric {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+}
