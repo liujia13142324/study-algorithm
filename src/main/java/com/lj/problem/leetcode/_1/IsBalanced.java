6f97aa2cf6448244cb1dff38bed4e7878b3371ca
wangdamao <524294514@qq.com>
2025-09-19 23:38:39 +0800

    [Question Salved] [来源]：leetcode [题目]：110. 平衡二叉树 [难度]：简单 [思路]：

---
+++ src/main/java/com/lj/problem/leetcode/_1/IsBalanced.java
@@ -0,0 +1,51 @@
+package com.lj.problem.leetcode._1;
+
+/**
+ * 110. 平衡二叉树
+ * 给定一个二叉树，判断它是否是 平衡二叉树
+ *
+ * 示例 1：
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ *
+ * 示例 3：
+ * 输入：root = []
+ * 输出：true
+ *
+ *
+ * 提示：
+ * 树中的节点数在范围 [0, 5000] 内
+ * -104 <= Node.val <= 104
+ */
+public class IsBalanced {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // TODO 自底向上写法
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        int minus = depth(root.left) - depth(root.right);
+        return Math.abs(minus) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+}
