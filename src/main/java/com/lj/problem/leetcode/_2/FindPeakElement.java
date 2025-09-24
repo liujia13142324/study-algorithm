0df2c5fafbf352d8d5f1fbd98f46c303df795aa9
wangdamao <524294514@qq.com>
2025-09-24 23:28:45 +0800

    [Question Salved] [来源]：leetcode [题目]：162. 寻找峰值 [难度]：中等 [思路]：两种循环不变量定义

--- src/main/java/com/lj/problem/leetcode/_2/FindPeakElement.java
+++ src/main/java/com/lj/problem/leetcode/_2/FindPeakElement.java
@@ -30,6 +30,29 @@ package com.lj.problem.leetcode._2;
  */
 public class FindPeakElement {
 
+    /**
+     * 循环不变量：
+     *  是 target 或 target 左侧，染蓝色
+     *  target 右侧，染红色
+     */
+    public int findPeakElement2(int[] nums) {
+        int r = nums.length - 1, l = 1;
+        while (l <= r) {
+            int mid = (l + r) >>> 1;
+            if (nums[mid] > nums[mid - 1]) {
+                l = mid + 1;
+            }else {
+                r = mid - 1;
+            }
+        }
+        return r;
+    }
+
+    /**
+     * 循环不变量：
+     *  是 target 或 target 右侧，染蓝色
+     *  target 左侧，染红色
+     */
     public int findPeakElement(int[] nums) {
         int l = 0, r = nums.length - 2;
         while (l <= r) {
