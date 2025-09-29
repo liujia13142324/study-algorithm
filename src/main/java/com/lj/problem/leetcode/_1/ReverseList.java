b64401dc8775cc33ea2ff16b40a93c9ca28236bc
wangdamao <524294514@qq.com>
2025-09-29 23:55:22 +0800

    [Question Salved] [来源]：leetcode [题目]：206. 反转链表 [难度]：简单 [思路]：递归

--- src/main/java/com/lj/problem/leetcode/_1/ReverseList.java
+++ src/main/java/com/lj/problem/leetcode/_1/ReverseList.java
@@ -1,11 +1,93 @@
 package com.lj.problem.leetcode._1;
 
+import org.junit.Test;
+
 /**
+ * 206. 反转链表
  * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ *
+ * 示例 2：
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ *
+ * 示例 3：
+ * 输入：head = []
+ * 输出：[]
+ *
+ * 提示：
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ *
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
  */
 public class ReverseList {
-    
-    
+
+    @Test
+    public void test() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ListNode reversed = reverseList2(head);
+        while (reversed != null) {
+            System.out.println(reversed.val);
+            reversed = reversed.next;
+        }
+    }
+
+    ListNode ans = null;
+    public ListNode reverseList2(ListNode head) {
+        if (head == null) return null;
+        traverse(head).next = null;
+        return ans;
+    }
+
+    private ListNode traverse(ListNode current) {
+        if (current.next == null) {
+            ans = current;
+            return current;
+        }
+        ListNode pre = traverse(current.next);
+        pre.next = current;
+        return current;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /**
      * 逆天的做法
      * @param head
