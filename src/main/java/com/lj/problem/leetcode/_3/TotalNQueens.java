7637a18acd50533f4b099cd8513ddbcb25341b9b
wangdamao <524294514@qq.com>
2025-11-11 23:58:20 +0800

    [Question Salved] [来源]：leetcode [题目]：52. N 皇后 II [难度]：困难 [思路]：

---
+++ src/main/java/com/lj/problem/leetcode/_3/TotalNQueens.java
@@ -0,0 +1,62 @@
+package com.lj.problem.leetcode._3;
+
+/**
+ * 52. N 皇后 II
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n × n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ * 给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
+ *
+ * 示例 1：
+ * 输入：n = 4
+ * 输出：2
+ * 解释：如上图所示，4 皇后问题存在两个不同的解法。
+ *
+ * 示例 2：
+ * 输入：n = 1
+ * 输出：1
+ *
+ * 提示：
+ * 1 <= n <= 9
+ */
+public class TotalNQueens {
+
+    public int totalNQueens(int n) {
+        boolean[] disabled = new boolean[n];
+        int[] path = new int[n];
+        return dfs(0, n, path, disabled);
+    }
+
+    public int dfs(int i, int n, int[] path, boolean[] disabled) {
+        if (i == n) {
+            return 1;
+        }
+
+        int ans = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (!disabled[j]) {
+                path[i] = j;
+                ans += dfs(i+1, n, path, checkDisabled(i+1, path));
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean[] checkDisabled(int i, int[] path) {
+        boolean[] result = new boolean[path.length];
+        for (int j = 0; j < i; j++) {
+            result[path[j]] = true;
+            if (path[j] - (i - j) >= 0) {
+                result[path[j] - (i - j)] = true;
+            }
+
+            if (path[j] + (i - j) < result.length) {
+                result[path[j] + (i - j)] = true;
+            }
+        }
+
+        return result;
+    }
+
+}
