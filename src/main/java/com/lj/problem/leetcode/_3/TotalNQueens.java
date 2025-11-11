33e8014c8aad579fe5ccbfb7d329e384f0be96b3
wangdamao <524294514@qq.com>
2025-11-12 02:01:23 +0800

    [Question Salved] [来源]：leetcode [题目]：52. N 皇后 II [难度]：困难 [思路]：

--- src/main/java/com/lj/problem/leetcode/_3/TotalNQueens.java
+++ src/main/java/com/lj/problem/leetcode/_3/TotalNQueens.java
@@ -1,5 +1,7 @@
 package com.lj.problem.leetcode._3;
 
+import org.junit.Test;
+
 /**
  * 52. N 皇后 II
  * n 皇后问题 研究的是如何将 n 个皇后放置在 n × n 的棋盘上，并且使皇后彼此之间不能相互攻击。
@@ -19,6 +21,33 @@ package com.lj.problem.leetcode._3;
  * 1 <= n <= 9
  */
 public class TotalNQueens {
+
+    @Test
+    public void test() {
+        System.out.println(totalNQueens(9));
+    }
+
+    public int totalNQueens2(int n) {
+        boolean[] vertical = new boolean[n];
+        boolean[] left = new boolean[n * 2];
+        boolean[] right = new boolean[n * 2];
+        return dfs(0, n, vertical, left, right);
+    }
+
+    public int dfs(int i, int n, boolean[] vertical, boolean[] left, boolean[] right) {
+        if (i == n) {
+            return 1;
+        }
+        int ans = 0;
+        for (int c = 0; c < n; c++) {
+           if (!vertical[c] && !left[i + c] && !right[i - c + n]) {
+               vertical[c] = left[i + c] = right[i - c + n] = true;
+               ans += dfs(i + 1, n, vertical, left, right);
+               vertical[c] = left[i + c] = right[i - c + n] = false;
+           }
+        }
+        return ans;
+    }
 
     public int totalNQueens(int n) {
         boolean[] disabled = new boolean[n];
