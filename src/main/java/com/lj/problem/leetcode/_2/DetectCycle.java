523b81d0ded5480cbf5ad36fd9406376a697d332
wangdamao <524294514@qq.com>
2025-11-15 23:57:29 +0800

    [Question Salved] [来源]：leetcode [题目]：142. 环形链表 II [难度]：中等 [思路]：

--- src/main/java/com/lj/problem/leetcode/_2/DetectCycle.java
+++ src/main/java/com/lj/problem/leetcode/_2/DetectCycle.java
@@ -6,7 +6,6 @@ package com.lj.problem.leetcode._2;
  * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
  * 不允许修改 链表。
  *
- *
  * 示例 1：
  * 输入：head = [3,2,0,-4], pos = 1
  * 输出：返回索引为 1 的链表节点
@@ -30,26 +29,34 @@ package com.lj.problem.leetcode._2;
  *
  * 进阶：你是否可以使用 O(1) 空间解决此题？
  *
- *
+ * tmp
  */
 public class DetectCycle {
 
     public ListNode detectCycle(ListNode head) {
-        ListNode s = head;
-        ListNode f = head;
-
-        while (f != null && f.next != null) {
-            f = f.next.next;
-            s = s.next;
-            if (f == s) {
-                while (s != head) {
-                    head = head.next;
-                    s = s.next;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                int d1 = 0;
+                int loopCnt = 0;
+                ListNode tmp = slow;
+                while (true) {
+                    if (fast.next == tmp || fast.next.next == tmp) {
+                        loopCnt++;
+                    }
+                    fast = fast.next.next;
+                    slow = slow.next;
+                    d1++;
+                    if (fast == slow) {
+                        
+                    }
                 }
-                return head;
+
             }
         }
-
         return null;
     }
 
@@ -62,3 +69,5 @@ public class DetectCycle {
         }
     }
 }
+
+
