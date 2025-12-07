e22a158d3ecd2691b2c176a1651353dc74cc0470
jia <524294514@qq.com>
2025-12-08 00:09:15 +0800

    [Question Salved] [来源]：leedcode [题目]：416. 分割等和子集 [难度]：中等 [思路]：

--- src/main/java/com/lj/problem/leetcode/_2/CanPartition.java
+++ src/main/java/com/lj/problem/leetcode/_2/CanPartition.java
@@ -2,6 +2,8 @@ package com.lj.problem.leetcode._2;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * 416. 分割等和子集
  * 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
@@ -20,7 +22,6 @@ import org.junit.Test;
  * 1 <= nums.length <= 200
  * 1 <= nums[i] <= 100
  *
- * tmp
  */
 public class CanPartition {
 
@@ -35,6 +36,29 @@ public class CanPartition {
         System.out.println(canPartition4(new int[]{1, 1}));
     }
 
+
+    public boolean canPartition5___(int[] nums) {
+        int sum = 0;
+        for (int num: nums) sum += num;
+        if ((sum & 1) != 0) return false;
+
+        int target = sum/2;
+        boolean[][] dp = new boolean[nums.length + 1][target + 1];
+        Arrays.fill(dp[0], false);
+        dp[0][0] = true;
+
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
+                }else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[nums.length][target];
+    }
 
     public boolean canPartition5__(int[] nums) {
         int sum = 0;
