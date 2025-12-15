f08efdffc924dcb71b58e387bf07710d14f2946e
wangdamao <524294514@qq.com>
2025-12-16 01:18:35 +0800

    [Question Salved] [来源]：leetcode [题目]：1143. 最长公共子序列 [难度]：中等 [思路]：

--- src/main/java/com/lj/problem/leetcode/_2/LongestCommonSubsequence.java
+++ src/main/java/com/lj/problem/leetcode/_2/LongestCommonSubsequence.java
@@ -39,6 +39,22 @@ public class LongestCommonSubsequence {
         System.out.println(longestCommonSubsequence("abcba", "abcbcba"));
     }
 
+    public int longestCommonSubsequence4__(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        char[] chars1 = text1.toCharArray();
+        char[] chars2 = text2.toCharArray();
+        for (int i = 1; i <= chars1.length; i++) {
+            for (int j = 1; j <= chars2.length; j++) {
+                if (chars1[i - 1] == chars2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[chars1.length][chars2.length];
+    }
+
     public int longestCommonSubsequence4_(String text1, String text2) {
         int[][] cache = new int[text1.length()][text2.length()];
         return dfs4_(text2.length() - 1, text1.length() - 1, text2.toCharArray(), text1.toCharArray(), cache);
