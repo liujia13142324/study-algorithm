90b2ebf70c72256ff4219bc15edc00a3a750b204
jia <524294514@qq.com>
2025-12-19 03:51:47 +0800

    [Question Salved] [来源]：leedcode [题目]：97. 交错字符串 [难度]：中等 [思路]：

---
+++ src/main/java/com/lj/problem/leetcode/_2/IsInterleave.java
@@ -0,0 +1,78 @@
+package com.lj.problem.leetcode._2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 97. 交错字符串
+ * 给定三个字符串 s1、s2、s3，请你帮忙验证 s3 是否是由 s1 和 s2 交错 组成的。
+ * 两个字符串 s 和 t 交错 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
+ *
+ * s = s1 + s2 + ... + sn
+ * t = t1 + t2 + ... + tm
+ * |n - m| <= 1
+ * 交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ...
+ * 注意：a + b 意味着字符串 a 和 b 连接。
+ *
+ *
+ * 示例 1：
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+ * 输出：false
+ *
+ * 示例 3：
+ * 输入：s1 = "", s2 = "", s3 = ""
+ * 输出：true
+ *
+ *
+ * 提示：
+ * 0 <= s1.length, s2.length <= 100
+ * 0 <= s3.length <= 200
+ * s1、s2、和 s3 都由小写英文字母组成
+ *
+ * 进阶：您能否仅使用 O(s2.length) 额外的内存空间来解决它?
+ */
+public class IsInterleave {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) return false;
+
+        char[] chars = s3.toCharArray();
+        List<Character> path = new ArrayList<>(chars.length);
+        Boolean[][] cache = new Boolean[s1.length() + 1][s2.length() + 1];
+        return dfs(s1.length(), s2.length(), s1.toCharArray(), s2.toCharArray(), chars, path, cache);
+    }
+
+    private boolean dfs(int i, int j, char[] s1, char[] s2, char[] s3, List<Character> path, Boolean[][] cache) {
+        if (i == 0 && j == 0) {
+            return path.size() == s3.length;
+        }
+        if (cache[i][j] != null) {
+            return cache[i][j];
+        }
+        int idx = s3.length - 1 - path.size();
+        if (i > 0 && s1[i - 1] == s3[idx]) {
+            path.add(s1[i - 1]);
+            if (dfs(i - 1, j, s1, s2, s3, path, cache)) {
+                cache[i][j] = true;
+                return true;
+            }
+            path.remove(path.size() - 1);
+        }
+
+        if (j > 0 && s2[j - 1] == s3[idx]) {
+            path.add(s2[j - 1]);
+            if (dfs(i, j - 1, s1, s2, s3, path, cache)) {
+                cache[i][j] = true;
+                return true;
+            }
+            path.remove(path.size() - 1);
+        }
+
+        cache[i][j] = false;
+        return false;
+    }
+}
