235c0701efea16de59197c360d603d685b399181
jia <524294514@qq.com>
2025-12-23 01:54:36 +0800

    [Question Salved] [来源]：leedcode [题目]：1092. 最短公共超序列 [难度]：困难 [思路]：

--- src/main/java/com/lj/problem/leetcode/_3/ShortestCommonSupersequence.java
+++ src/main/java/com/lj/problem/leetcode/_3/ShortestCommonSupersequence.java
@@ -34,8 +34,51 @@ public class ShortestCommonSupersequence {
 
     @Test
     public void test() {
-        System.out.println(shortestCommonSupersequence__("abac", "cab"));
-        System.out.println(shortestCommonSupersequence__("aaaaaaaa", "aaaaaaaa"));
+        System.out.println(shortestCommonSupersequence___("abac", "cab"));
+        System.out.println(shortestCommonSupersequence___("aaaaaaaa", "aaaaaaaa"));
+    }
+
+    public String shortestCommonSupersequence___(String str1, String str2) {
+        char[] chars1 = str1.toCharArray();
+        char[] chars2 = str2.toCharArray();
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        for (int i = 1; i <= chars2.length; i++) dp[0][i] = i;
+        for (int i = 1; i <= chars1.length; i++) dp[i][0] = i;
+
+        for (int i = 1; i <= chars1.length; i++) {
+            for (int j = 1; j <= chars2.length; j++) {
+                if (chars1[i - 1] == chars2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + 1;
+                }
+            }
+        }
+
+        char[] ans = new char[dp[chars1.length][chars2.length]];
+        int idx = ans.length - 1;
+        int i = chars1.length - 1;
+        int j = chars2.length - 1;
+
+        while (i >= 0 || j >= 0) {
+            if (i < 0) {
+                System.arraycopy(chars2, 0, ans, 0, idx + 1);
+                break;
+            }
+            if (j < 0) {
+                System.arraycopy(chars1, 0, ans, 0, idx + 1);
+                break;
+            }
+            if (chars1[i] == chars2[j]) {
+                ans[idx --] = chars1[i];
+                i--; j--;
+            }else if (dp[i][j + 1] < dp[i + 1][j]) {
+                ans[idx--] = chars1[i--];
+            }else {
+                ans[idx--] = chars2[j--];
+            }
+        }
+        return new String(ans);
     }
 
     public String shortestCommonSupersequence__(String str1, String str2) {
