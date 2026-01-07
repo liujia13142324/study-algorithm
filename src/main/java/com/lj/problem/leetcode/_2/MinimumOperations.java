c48e35e7c87a7612f96d2a157e8bd02aadbae38b
jia <524294514@qq.com>
2026-01-08 00:15:59 +0800

    [Question Salved] [来源]：leedcode [题目]：2826. 将三个组排序 [难度]：中等 [思路]：

--- src/main/java/com/lj/problem/leetcode/_2/MinimumOperations.java
+++ src/main/java/com/lj/problem/leetcode/_2/MinimumOperations.java
@@ -43,6 +43,21 @@ public class MinimumOperations {
         System.out.println(minimumOperations2(Arrays.asList(2,2,2,2,3,3)));
     }
 
+    public int minimumOperations3(List<Integer> nums) {
+        int[] dp = new int[4];
+        int max = 0;
+        for (int num: nums) {
+            int tmp = 0;
+            for (int i = 1; i <= num; i++) {
+                tmp = Math.max(tmp, dp[i]);
+            }
+            dp[num] = tmp + 1;
+            max = Math.max(max, dp[num]);
+        }
+
+        return nums.size() - max;
+    }
+
     public int minimumOperations2_(List<Integer> nums) {
         int[] dp = new int[4];
         for (int num: nums) {
