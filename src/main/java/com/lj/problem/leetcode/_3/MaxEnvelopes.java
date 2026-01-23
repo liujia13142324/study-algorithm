cdde4cf30d72992b3fa0618ae9277ebeb19776af
jia <524294514@qq.com>
2026-01-23 23:54:42 +0800

    [Question Salved] [来源]：leedcode [题目]：354. 俄罗斯套娃信封问题 [难度]：困难 [思路]：

--- src/main/java/com/lj/problem/leetcode/_3/MaxEnvelopes.java
+++ src/main/java/com/lj/problem/leetcode/_3/MaxEnvelopes.java
@@ -47,6 +47,27 @@ public class MaxEnvelopes {
      * @param envelopes
      * @return
      */
+    public int maxEnvelopes2__(int[][] envelopes) {
+        Arrays.sort(envelopes, Comparator.comparingInt(a -> a[0]));
+        int[] dp = new int[envelopes.length];
+        Arrays.fill(dp, 1);
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < envelopes.length; i++) {
+           for (int j = 0; j < i; j++) {
+               if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]) {
+                   dp[i] = Math.max(dp[i], dp[j] + 1);
+               }
+           }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    /**
+     * 85/87
+     * @param envelopes
+     * @return
+     */
     public int maxEnvelopes2_(int[][] envelopes) {
         Arrays.sort(envelopes, Comparator.comparingInt(a -> a[0]));
         int[] cache = new int[envelopes.length];
