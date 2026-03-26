9d45c0361e319619d081e43aaffb64c196192f69
jia <524294514@qq.com>
2026-03-27 00:03:18 +0800

    [Question Salved] [来源]：leedcode [题目]：122. 买卖股票的最佳时机 II [难度]：中等 [思路]：

--- src/main/java/com/lj/problem/leetcode/_2/MaxProfit2.java
+++ src/main/java/com/lj/problem/leetcode/_2/MaxProfit2.java
@@ -1,6 +1,7 @@
 package com.lj.problem.leetcode._2;
 
 /**
+ * 122. 买卖股票的最佳时机 II
  * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
  *
  * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。然而，你可以在 同一天 多次买卖该股票，但要确保你持有的股票不超过一股。
@@ -47,7 +48,7 @@ public class MaxProfit2 {
         if (hold == 0) {
             return Math.max(dfs(i + 1, 0, sum, prices), dfs(i + 1, 1, sum - prices[i], prices));
         }else {
-            return Math.max(dfs(i + 1, 0, sum + prices[i], prices), dfs(i + 1, 1, prices[i], prices));
+            return Math.max(dfs(i + 1, 0, sum + prices[i], prices), dfs(i + 1, 1, sum, prices));
         }
     }
 }
