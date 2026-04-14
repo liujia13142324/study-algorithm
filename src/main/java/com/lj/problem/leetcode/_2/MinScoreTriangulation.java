08a7df587a5e572618869a1c4ff227f4ac684d9e
jia <524294514@qq.com>
2026-04-14 23:54:42 +0800

    [Question Salved] [来源]：leedcode [题目]：1039. 多边形三角剖分的最低得分 [难度]：中等 [思路]：

---
+++ src/main/java/com/lj/problem/leetcode/_2/MinScoreTriangulation.java
@@ -0,0 +1,22 @@
+package com.lj.problem.leetcode._2;
+
+/**
+ * 1039. 多边形三角剖分的最低得分
+ * 提示
+ * 你有一个凸的 n 边形，其每个顶点都有一个整数值。给定一个整数数组 values ，其中 values[i] 是按 顺时针顺序 第 i 个顶点的值。
+ * 假设将多边形 剖分 为 n - 2 个三角形。对于每个三角形，该三角形的值是顶点标记的乘积，三角剖分的分数是进行三角剖分后所有 n - 2 个三角形的值之和。
+ * 返回 多边形进行三角剖分后可以得到的最低分 。
+ *
+ * 提示：
+ *
+ * n == values.length
+ * 3 <= n <= 50
+ * 1 <= values[i] <= 100
+ */
+public class MinScoreTriangulation {
+
+    public int minScoreTriangulation(int[] values) {
+        // n - 2 个三角形，需要
+    }
+
+}
