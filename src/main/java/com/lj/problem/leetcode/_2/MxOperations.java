b1f7e58e37809c6a506b5079ae5a950df88e8ab8
jia <524294514@qq.com>
2026-04-19 16:17:33 +0800

    [Question Salved] [来源]：leedcode [题目]：3040. 相同分数的最大操作数目 II [难度]：中等 [思路]：

---
+++ src/main/java/com/lj/problem/leetcode/_2/MxOperations.java
@@ -0,0 +1,67 @@
+package com.lj.problem.leetcode._2;
+
+import org.junit.Test;
+
+/**
+ * 3040. 相同分数的最大操作数目 II
+ * 给你一个整数数组 nums ，如果 nums 至少 包含 2 个元素，你可以执行以下操作中的 任意 一个：
+ * 选择 nums 中最前面两个元素并且删除它们。
+ * 选择 nums 中最后两个元素并且删除它们。
+ * 选择 nums 中第一个和最后一个元素并且删除它们。
+ * 一次操作的 分数 是被删除元素的和。
+ * 在确保 所有操作分数相同 的前提下，请你求出 最多 能进行多少次操作。
+ * 请你返回按照上述要求 最多 可以进行的操作次数。
+ *
+ * 示例 1：
+ * 输入：nums = [3,2,1,2,3,4]
+ * 输出：3
+ * 解释：我们执行以下操作：
+ * - 删除前两个元素，分数为 3 + 2 = 5 ，nums = [1,2,3,4] 。
+ * - 删除第一个元素和最后一个元素，分数为 1 + 4 = 5 ，nums = [2,3] 。
+ * - 删除第一个元素和最后一个元素，分数为 2 + 3 = 5 ，nums = [] 。
+ * 由于 nums 为空，我们无法继续进行任何操作。
+
+ * 示例 2：
+ * 输入：nums = [3,2,6,1,4]
+ * 输出：2
+ * 解释：我们执行以下操作：
+ * - 删除前两个元素，分数为 3 + 2 = 5 ，nums = [6,1,4] 。
+ * - 删除最后两个元素，分数为 1 + 4 = 5 ，nums = [6] 。
+ * 至多进行 2 次操作。
+ *
+ * 提示：
+ * 2 <= nums.length <= 2000
+ * 1 <= nums[i] <= 1000
+ */
+public class MxOperations {
+
+    @Test
+    public void test() {
+        System.out.println(maxOperations(new int[]{1,9,7,3,2,7,4,12,2,6}));
+    }
+
+    public int maxOperations(int[] nums) {
+        return 1 + Math.max(
+                dfs(2, nums.length - 1, nums[0] + nums[1], nums),
+                Math.max(
+                        dfs(0, nums.length - 3, nums[nums.length - 1] + nums[nums.length - 2], nums),
+                        dfs(1, nums.length - 2, nums[0] + nums[nums.length - 1], nums)
+                )
+        );
+    }
+
+    private int dfs(int i, int j, int k, int[] nums) {
+        if (i >= j) return 0;
+        int ans = 0;
+        if (i + 1 <= j && nums[i] + nums[i + 1] == k) {
+            ans = dfs(i + 2, j, k, nums) + 1 ;
+        }
+        if (j - 1 >= i && nums[j] + nums[j - 1] == k) {
+            ans = Math.max(ans, dfs(i, j - 2, k, nums) + 1);
+        }
+        if (nums[i] + nums[j] == k) {
+            ans = Math.max(ans, dfs(i + 1, j - 1, k, nums) + 1);
+        }
+        return ans;
+    }
+}
