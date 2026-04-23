ae27b1e604d92d593795fff984fa578540784153
jia <524294514@qq.com>
2026-04-23 22:25:01 +0800

    [Question Salved] [来源]：leedcode [题目]：1770. 执行乘法运算的最大分数 [难度]：困难 [思路]：

--- src/main/java/com/lj/problem/leetcode/_3/MaximumScore.java
+++ src/main/java/com/lj/problem/leetcode/_3/MaximumScore.java
@@ -1,5 +1,7 @@
 package com.lj.problem.leetcode._3;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 /**
@@ -41,6 +43,40 @@ import java.util.Arrays;
  */
 public class MaximumScore {
 
+    @Test
+    public void test() {
+//        System.out.println(maximumScore_(new int[]{1,2,3}, new int[]{3,2,1}));
+        System.out.println(maximumScore_(
+                new int[]{830,388,289,14,-221,610,163,444,-750,-217,-235,-882,746,-907,67,-71,-990,400,-54,-114,-100,233,667,612,-812,-471,-639,-306,-95,524,-198,-520,-652,704,-697,-43,-539,-105,-160,838,499,-109,-977,975,658,-250,593,154,-777,496,747,307,-340,403,-749,-185,721,327,-851,-112,770,-14,754,-242,-220,-776,-66,348,-707,-693,9,-354,741,129,234,638,404,-408,-30,422,-234,818,-627,706,-752,-699,-773,-786,241,432,273,272,-57,878,-149,-547,491,519,870,700,476,-99,902,-878,-237,-549,445,-372,277,-486,872,-569,-687,339,653,-564,862,898,-962,306,668,-143,344,-312,108,-536,-453,-52,627,-225,-28,403,-422,367,133,970,-575,353,347,275,-876,337,594,441,-498,-875,-934,133}
+                , new int[]{-844,-363,797,480,141,733,-935,842,160,-928,787,-895,-742,-963,889,-713,-264,-400,117,163,68,-286,-810,-365,180,-690,-558,-409,290,51,-272,-454,-110,850,578,131,-913,675,817,380,410,860,-441,56,-80,-650,-474,858,269})
+        );
+    }
+
+    public int maximumScore_(int[] nums, int[] multipliers) {
+        int[][] dp = new int[nums.length][multipliers.length + 1];
+        int[] tmp = new int[nums.length];
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int[] prev = new int[nums.length];
+            for (int k = multipliers.length - 1; k >= 0; k--) {
+                dp[i][k] = nums[i] * multipliers[k];
+                for (int j = nums.length - 1; j > i; j--) {
+                    tmp[j] = dp[j][k];
+                    dp[j][k] = Math.max(prev[j] + nums[i] * multipliers[k], dp[j - 1][k + 1] + nums[j] * multipliers[k]);
+                }
+                System.arraycopy(tmp, i + 1, prev, i + 1, nums.length - i - 1);
+            }
+        }
+
+        return dp[nums.length - 1][0];
+    }
+
+    /**
+     * 内存超了
+     * @param nums
+     * @param multipliers
+     * @return
+     */
     public int maximumScore(int[] nums, int[] multipliers) {
         int[][][] cache = new int[nums.length][nums.length][multipliers.length];
         for (int[][] c1: cache) {
