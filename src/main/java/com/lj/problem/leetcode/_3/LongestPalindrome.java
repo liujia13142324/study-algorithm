b3f71a7a6f1acb85bc0e210129741cbea42b9777
jia <524294514@qq.com>
2026-04-26 19:33:28 +0800

    [Question Salved] [来源]：leedcode [题目]：1771. 由子序列构造的最长回文串的长度 [难度]：困难 [思路]：

--- src/main/java/com/lj/problem/leetcode/_3/LongestPalindrome.java
+++ src/main/java/com/lj/problem/leetcode/_3/LongestPalindrome.java
@@ -41,9 +41,62 @@ import java.util.Arrays;
  * word1 和 word2 由小写英文字母组成
  */
 public class LongestPalindrome {
+    int[][] cache1;
+    int[][] cache2;
+
+    public int longestPalindrome2(String word1, String word2) {
+        char[] chars1 = word1.toCharArray();
+        char[] chars2 = word2.toCharArray();
+        int[][][] cache = new int[2][chars1.length][chars2.length];
+        cache1 = new int[word1.length()][word1.length()];
+        cache2 = new int[word2.length()][word2.length()];
+        for (int[] tmp: cache[0]) Arrays.fill(tmp, -1);
+        for (int[] tmp: cache[1]) Arrays.fill(tmp, -1);
+        return dfs2(0, chars2.length-1, 0, chars1, chars2, cache);
+    }
+
+    private int dfs2(int i, int j, int pass, char[] chars1, char[] chars2, int[][][] cache) {
+        if (i >= chars1.length || j < 0) {
+            if (pass == 0) {
+                return 0;
+            }
+            if (i >= chars1.length) {
+                return dfs(0, j, chars2, cache2);
+            }else {
+                return dfs(i, chars1.length - 1, chars1, cache1);
+            }
+        }
+
+        if (cache[pass][i][j] != -1) {
+            return cache[pass][i][j];
+        }
+
+        if (chars1[i] == chars2[j]) {
+            return cache[pass][i][j] = dfs2(i + 1, j - 1, 1, chars1, chars2, cache) + 2;
+        }
+
+        return cache[pass][i][j] = Math.max(dfs2(i + 1, j, pass, chars1, chars2, cache), dfs2(i, j - 1, pass, chars1, chars2, cache));
+    }
+
+    private int dfs(int i, int j, char[] chars, int[][] cache) {
+        if (i > j) {
+            return 0;
+        }
+        if (i == j) {
+            return 1;
+        }
+        if (cache[i][j] != 0) {
+            return cache[i][j];
+        }
+        if (chars[i] == chars[j]) {
+            return cache[i][j] = dfs(i + 1, j - 1, chars, cache) + 2;
+        }
+        return cache[i][j] = Math.max(dfs(i + 1, j , chars, cache), dfs(i, j - 1, chars, cache));
+    }
 
     int t1;
     int t2;
+
     public int longestPalindrome(String word1, String word2) {
         char[] chars = (word1 + word2).toCharArray();
         t1 = word1.length();
@@ -75,6 +128,4 @@ public class LongestPalindrome {
 
         return cache[pass][i][j] = Math.max(dfs(i + 1, j, pass, chars, cache), dfs(i, j - 1, pass, chars, cache));
     }
-
-
 }
