7e44c77aaa6a802eb3c5172cf8586d900ead514c
jia <524294514@qq.com>
2026-04-29 23:56:48 +0800

    [Question Salved] [来源]：leedcode [题目]：1547. 切棍子的最小成本 [难度]：困难 [思路]：

--- src/main/java/com/lj/problem/leetcode/_3/MinCost.java
+++ src/main/java/com/lj/problem/leetcode/_3/MinCost.java
@@ -32,6 +32,27 @@ import java.util.Arrays;
  */
 public class MinCost {
 
+    public int minCost3(int n, int[] cuts) {
+        Arrays.sort(cuts);
+        int m = cuts.length + 2;
+        int[] newCuts = new int[m];
+        System.arraycopy(cuts, 0, newCuts, 1, m - 2);
+        newCuts[m - 1] = n;
+
+        int[][] f = new int[m][m];
+        for (int i = m - 3; i >= 0; i--) {
+            for (int j = i + 2; j < m; j++) {
+                int res = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++) {
+                    res = Math.min(res, f[i][k] + f[k][j]);
+                }
+                f[i][j] = res + newCuts[j] - newCuts[i];
+            }
+        }
+        return f[0][m - 1];
+    }
+
+
     public int minCost(int n, int[] cuts) {
         Arrays.sort(cuts);
         int[] tmp = new int[cuts.length + 2];
