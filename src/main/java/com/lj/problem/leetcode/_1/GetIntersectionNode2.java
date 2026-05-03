1e9fdea9ff0f6ff5dba99eca70009315371b68b5
jia <524294514@qq.com>
2026-05-04 00:22:32 +0800

    [Question Salved] [来源]：leedcode [题目]：160. 相交链表 [难度]：简单 [思路]：

--- src/main/java/com/lj/problem/leetcode/_1/GetIntersectionNode2.java
+++ src/main/java/com/lj/problem/leetcode/_1/GetIntersectionNode2.java
@@ -56,6 +56,16 @@ package com.lj.problem.leetcode._1;
  */
 public class GetIntersectionNode2 {
 
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode a = headA;
         ListNode b = headB;
