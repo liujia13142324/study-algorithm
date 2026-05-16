2d998157f35a3df4c6f6c605665cf17558ac3524
wangdamao <524294514@qq.com>
2026-05-16 23:56:42 +0800

    [Question Salved] [来源]：leetcode [题目]：217. 存在重复元素 [难度]：简单 [思路]：

--- src/main/java/com/lj/problem/leetcode/_1/ContainsDuplicate.java
+++ src/main/java/com/lj/problem/leetcode/_1/ContainsDuplicate.java
@@ -1,5 +1,8 @@
 package com.lj.problem.leetcode._1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 217. 存在重复元素
  * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
@@ -28,7 +31,12 @@ package com.lj.problem.leetcode._1;
 public class ContainsDuplicate {
 
     public boolean containsDuplicate(int[] nums) {
-
+        Set<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            if (set.contains(num)) return true;
+            set.add(num);
+        }
+        return false;
     }
 
 }
