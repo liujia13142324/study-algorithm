3c5955980f9834a0de8963b34a135c9780487d92
jia <524294514@qq.com>
2026-05-17 16:50:28 +0800

    [Question Salved] [来源]：leedcode [题目]：219. 存在重复元素 II [难度]：简单 [思路]：

--- src/main/java/com/lj/problem/leetcode/_1/ContainsNearbyDuplicate.java
+++ src/main/java/com/lj/problem/leetcode/_1/ContainsNearbyDuplicate.java
@@ -1,5 +1,8 @@
 package com.lj.problem.leetcode._1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 219. 存在重复元素 II
  * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
@@ -22,7 +25,18 @@ package com.lj.problem.leetcode._1;
  * 0 <= k <= 105
  */
 public class ContainsNearbyDuplicate {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
 
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> mapping = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            Integer j = mapping.get(num);
+            if (j != null && (i - j) <= k) {
+                return true;
+            } else {
+                mapping.put(num, i);
+            }
+        }
+        return false;
     }
 }
