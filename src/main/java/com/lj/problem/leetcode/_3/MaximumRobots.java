1d8d94984f6d2c2f316e48a92c6f34b6fe942fc7
jia <524294514@qq.com>
2026-07-01 08:20:46 +0800

    [Question Salved] [来源]：leedcode [题目]：2398. 预算内的最多机器人数目 [难度]：困难 [思路]：

--- src/main/java/com/lj/problem/leetcode/_3/MaximumRobots.java
+++ src/main/java/com/lj/problem/leetcode/_3/MaximumRobots.java
@@ -1,5 +1,7 @@
 package com.lj.problem.leetcode._3;
 
+import org.junit.Test;
+
 /**
  * 2398. 预算内的最多机器人数目
  * 困难
@@ -38,7 +40,38 @@ package com.lj.problem.leetcode._3;
  */
 public class MaximumRobots {
 
-    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
+    @Test
+    public void test() {
+//        System.out.println(maximumRobots(new int[]{11,12,19}, new int[]{10,8,7}, 19));
+//        System.out.println(maximumRobots(new int[]{3,6,1,3,4}, new int[]{2,1,3,4,5}, 25));
+        System.out.println(maximumRobots(new int[]{19,63,21,8,5,46,56,45,54,30,92,63,31,71,87,94,67,8,19,89,79,25}
+                , new int[]{91,92,39,89,62,81,33,99,28,99,86,19,5,6,19,94,65,86,17,10,8,42}, 85));
+    }
 
+    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
+        long[] sum = new long[runningCosts.length + 1];
+        for (int i = 1; i <= runningCosts.length; i++) {
+            sum[i] = sum[i - 1] + runningCosts[i - 1];
+        }
+
+        int[] queue = new int[chargeTimes.length];
+        int head = 0, tail = -1;
+        int l = 0;
+        int ans = 0;
+        for (int i = 0; i < chargeTimes.length; i++) {
+            while (head <= tail && chargeTimes[queue[tail]] <= chargeTimes[i]) {
+                tail--;
+            }
+            queue[++tail] = i;
+
+            while (l <= i && chargeTimes[queue[head]] + (long) (i - l + 1) * (sum[i + 1] - sum[l]) > budget) {
+                if (l == queue[head]) {
+                    head++;
+                }
+                l++;
+            }
+            ans = Math.max(ans, i - l + 1);
+        }
+        return ans;
     }
 }
