3af9c4346695184f16785daf41fd32474d33a58c
jia <524294514@qq.com>
2026-07-18 16:33:30 +0800

    [Question Salved] [来源]：leedcode [题目]：739. 每日温度 [难度]：中等 [思路]：

---
+++ src/main/python/leecode/2_middle/dailyTemperatures.py
@@ -0,0 +1,49 @@
+# 739. 每日温度
+# 已解答
+# 中等
+# 相关标签
+# premium lock icon
+# 相关企业
+# 提示
+# 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+#
+#
+#
+# 示例 1:
+#
+# 输入: temperatures = [73,74,75,71,69,72,76,73]
+# 输出: [1,1,4,2,1,1,0,0]
+# 示例 2:
+#
+# 输入: temperatures = [30,40,50,60]
+# 输出: [1,1,1,0]
+# 示例 3:
+#
+# 输入: temperatures = [30,60,90]
+# 输出: [1,1,0]
+#
+#
+# 提示：
+#
+# 1 <= temperatures.length <= 105
+# 30 <= temperatures[i] <= 100
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = [0] * len(temperatures)
+        ans = [0] * len(temperatures)
+        top = -1
+        for i, val in enumerate(temperatures):
+            while top >= 0 and temperatures[stack[top]] < val:
+                tmp = stack[top]
+                ans[tmp] = i - tmp
+                top -= 1
+            top += 1
+            stack[top] = i
+        return ans
+
+
+if __name__ == '__main__':
+    print(Solution().dailyTemperatures([73,74,75,71,69,72,76,73]))
