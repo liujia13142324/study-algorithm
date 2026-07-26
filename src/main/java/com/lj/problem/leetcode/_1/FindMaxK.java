c6b13e79a9e9cbfd2d4084d17a90e7a75e79f2b3
jia <524294514@qq.com>
2026-07-26 22:53:09 +0800

    [Question Salved] [来源]：leedcode [题目]：1512. 好数对的数目 [难度]：简单 [思路]：

---
+++ src/main/java/com/lj/problem/leetcode/_1/FindMaxK.java
@@ -0,0 +1,69 @@
+package com.lj.problem.leetcode._1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 2441. 与对应负数同时存在的最大正整数
+ * 简单
+ * 相关标签
+ * premium lock icon
+ * 相关企业
+ * 提示
+ * 给你一个 不包含 任何零的整数数组 nums ，找出自身与对应的负数都在数组中存在的最大正整数 k 。
+ *
+ * 返回正整数 k ，如果不存在这样的整数，返回 -1 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-1,2,-3,3]
+ * 输出：3
+ * 解释：3 是数组中唯一一个满足题目要求的 k 。
+ * 示例 2：
+ *
+ * 输入：nums = [-1,10,6,7,-7,1]
+ * 输出：7
+ * 解释：数组中存在 1 和 7 对应的负数，7 的值更大。
+ * 示例 3：
+ *
+ * 输入：nums = [-10,8,6,7,-2,-3]
+ * 输出：-1
+ * 解释：不存在满足题目要求的 k ，返回 -1 。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 1000
+ * -1000 <= nums[i] <= 1000
+ * nums[i] != 0
+ */
+public class FindMaxK {
+
+    public int findMaxK2(int[] nums) {
+        int[] mapping = new int[2001];
+        int ans = -1;
+        for (int num: nums) {
+            if (mapping[1000 - num] != 0) {
+                ans = Math.max(ans, Math.abs(num));
+            }else {
+                mapping[num + 1000] = 1;
+            }
+        }
+        return ans;
+    }
+
+    public int findMaxK(int[] nums) {
+        Set<Integer> mapping = new HashSet();
+        int ans = -1;
+        for (int num: nums) {
+            if (mapping.contains(-num)) {
+                ans = Math.max(ans, Math.abs(num));
+            }else {
+                mapping.add(num);
+            }
+        }
+        return ans;
+    }
+}
