0c5f10c2630f9151ce01b88fae3bf7baffdfcad3
jia <524294514@qq.com>
2026-07-28 01:03:49 +0800

    [Question Salved] [来源]：leedcode [题目]：3880. 两个值之间的最小绝对差值 [难度]：简单 [思路]：

---
+++ src/main/python/leecode/2_middle/maxDistance.py
@@ -0,0 +1,44 @@
+# 624. 数组列表中的最大距离
+# 已解答
+# 中等
+# 相关标签
+# premium lock icon
+# 相关企业
+# 给定 m 个数组，每个数组都已经按照升序排好序了。
+#
+# 现在你需要从两个不同的数组中选择两个整数（每个数组选一个）并且计算它们的距离。两个整数 a 和 b 之间的距离定义为它们差的绝对值 |a-b| 。
+#
+# 返回最大距离。
+#
+# 示例 1：
+#
+# 输入：[[1,2,3],[4,5],[1,2,3]]
+# 输出：4
+# 解释：
+# 一种得到答案 4 的方法是从第一个数组或者第三个数组中选择 1，同时从第二个数组中选择 5 。
+# 示例 2：
+#
+# 输入：arrays = [[1],[1]]
+# 输出：0
+#
+#
+# 提示：
+#
+# m == arrays.length
+# 2 <= m <= 105
+# 1 <= arrays[i].length <= 500
+# -104 <= arrays[i][j] <= 104
+# arrays[i] 以 升序 排序。
+# 所有数组中最多有 105 个整数。
+
+
+class Solution(object):
+    def maxDistance(self, arrays):
+        min_val = arrays[0][0]
+        max_val = arrays[0][-1]
+        ans = 0
+        for i in range(1, len(arrays)):
+            ans = max(ans, max(abs(arrays[i][0] - max_val), abs(arrays[i][-1] - min_val)))
+            min_val = min(min_val, arrays[i][0])
+            max_val = max(max_val, arrays[i][-1])
+        return ans
