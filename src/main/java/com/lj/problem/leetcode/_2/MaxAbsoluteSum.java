cf0f4eed9cde0de0f422bafdaf05ead600667e42
jia <524294514@qq.com>
2026-07-30 00:54:32 +0800

    [Question Salved] [来源]：leedcode [题目]：1749. 任意子数组和的绝对值的最大值 [难度]：中等 [思路]：

--- src/main/java/com/lj/problem/leetcode/_2/MaxAbsoluteSum.java
+++ src/main/java/com/lj/problem/leetcode/_2/MaxAbsoluteSum.java
@@ -37,6 +37,16 @@ package com.lj.problem.leetcode._2;
 public class MaxAbsoluteSum {
 
     public int maxAbsoluteSum(int[] nums) {
+        int[][] dp = new int[2][nums.length];
+        dp[0][0] = dp[1][0] = nums[0];
+        int ans = Math.abs(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
 
+            dp[0][i] = Math.max(dp[0][i - 1] + nums[i], nums[i]);
+            dp[1][i] = Math.min(dp[1][i - 1] + nums[i], nums[i]);
+
+            ans = Math.max(ans, Math.max(Math.abs(dp[0][i]), Math.abs(dp[1][i])));
+        }
+        return ans;
     }
 }
