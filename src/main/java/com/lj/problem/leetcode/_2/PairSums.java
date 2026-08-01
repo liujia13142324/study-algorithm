76225d4fdb720927f64dea138d469cf9e8e74fbb
jia <524294514@qq.com>
2026-08-01 09:48:46 +0800

    [Question Salved] [来源]：leedcode [题目]：面试题 16.24. 数对和 [难度]：中等 [思路]：

---
+++ src/main/java/com/lj/problem/leetcode/_2/PairSums.java
@@ -0,0 +1,33 @@
+package com.lj.problem.leetcode._2;
+
+import java.util.List;
+
+/**
+ * 面试题 16.24. 数对和
+ * 中等
+ * 相关标签
+ * premium lock icon
+ * 相关企业
+ * 提示
+ * 设计一个算法，找出数组中两数之和为指定值的所有整数对。一个数只能属于一个数对。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [5,6,5], target = 11
+ * 输出：[[5,6]]
+ * 示例 2：
+ *
+ * 输入：nums = [5,6,5,6], target = 11
+ * 输出：[[5,6],[5,6]]
+ * 提示：
+ *
+ * nums.length <= 100000
+ * -105 <= nums[i], target <= 105
+ */
+public class PairSums {
+
+    public List<List<Integer>> pairSums(int[] nums, int target) {
+
+    }
+
+}
